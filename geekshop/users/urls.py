22145537c94bd3130ee21d6d1590525f060f3365
Vitaly <kazancevvitallijj@gmail.com>
2021-07-02 20:39:46 +0800

    comment CBV, return functions in users/views.py

--- geekshop/users/urls.py
+++ geekshop/users/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
-from users.views import GeekShopLogin, GeekShopRegister, GeekShopLogout, GeekShopProfile
+from users.views import login, register, logout, profile
 
 app_name = 'users'
 
 urlpatterns = [
-    path('login/', GeekShopLogin.as_view(), name='login'),
-    path('register/', GeekShopRegister.as_view(), name='register'),
-    path('logout/', GeekShopLogout.as_view(), name='logout'),
-    path('profile/,<int:pk>/', GeekShopProfile.as_view(), name='profile'),
+    path('login/', login, name='login'),
+    path('register/', register, name='register'),
+    path('logout/', logout, name='logout'),
+    path('profile/', profile, name='profile'),
 ]
--- geekshop/users/views.py
+++ geekshop/users/views.py
@@ -13,109 +13,110 @@ from users.models import User
 from baskets.models import Basket
 
 
-class GeekShopLogin(LoginView):
-    authentication_form = UserLoginForm
-    template_name = 'users/login.html'
-
-    def get_context_data(self, **kwargs):
-        context = super(GeekShopLogin, self).get_context_data()
-        context['title'] = 'GeekShop - Авторизация'
-        return context
-
-
-class GeekShopRegister(CreateView):
-    model = User
-    template_name = 'users/register.html'
-    form_class = UserRegisterForm
-    success_url = reverse_lazy('users:login')
-
-    def get_context_data(self, **kwargs):
-        context = super(GeekShopRegister, self).get_context_data()
-        context['title'] = 'GeekShop - Регистрация'
-        return context
+def login(request):
+    if request.method == 'POST':
+        form = UserLoginForm(data=request.POST)
+        if form.is_valid():
+            username = request.POST['username']
+            password = request.POST['password']
+            user = auth.authenticate(username=username, password=password)
+            if user and user.is_active:
+                auth.login(request, user)
+                return HttpResponseRedirect(reverse('index'))
+    else:
+        form = UserLoginForm()
+    context = {
+        'title': 'GeekShop - Авторизация',
+        'form': form,
+    }
+    return render(request, 'users/login.html', context)
 
 
-class GeekShopProfile(UpdateView):
-    model = User
-    template_name = 'users/profile.html'
-    form_class = UserProfileForm
-    # success_url = reverse_lazy('users:profile')
+def register(request):
+    if request.method == 'POST':
+        form = UserRegisterForm(data=request.POST)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Вы успешно зарегестрировались!')
+            return HttpResponseRedirect(reverse('users:login'))
+    else:
+        form = UserRegisterForm()
+    context = {
+        'title': 'GeekShop - Регистрация',
+        'form': form
+    }
+    return render(request, 'users/register.html', context)
 
-    def get_context_data(self, object_list=None, **kwargs):
-        context = super(GeekShopProfile, self).get_context_data()
-        context.update({
-            'title': 'GeekShop - Личный кабинет',
-            'baskets': Basket.objects.filter(user=self.request.user)
-        })
-        # context['title'] = 'GeekShop - Личный кабинет'
-        return context
 
-    def get_success_url(self):
-        return reverse_lazy('users:profile', args=(self.object.id,))
+@login_required()
+def profile(request):
+    user = request.user
+    if request.method == 'POST':
+        form = UserProfileForm(data=request.POST, files=request.FILES, instance=user)
+        if form.is_valid():
+            form.save()
+            return HttpResponseRedirect(reverse('users:profile'))
+        else:
+            print(form.errors)
+    else:
+        form = UserProfileForm(instance=user)
+    context = {
+        'title': 'GeekShop - Личный кабинет',
+        'form': form,
+        'baskets': Basket.objects.filter(user=user)
+    }
+    return render(request, 'users/profile.html', context)
 
-    @method_decorator(login_required())
-    def dispatch(self, request, *args, **kwargs):
-        return super(GeekShopProfile, self).dispatch(request, *args, **kwargs)
+
+def logout(request):
+    auth.logout(request)
+    return HttpResponseRedirect(reverse('index'))
+
+
+# class GeekShopLogin(LoginView):
+#     authentication_form = UserLoginForm
+#     template_name = 'users/login.html'
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(GeekShopLogin, self).get_context_data()
+#         context['title'] = 'GeekShop - Авторизация'
+#         return context
+#
+#
+# class GeekShopRegister(CreateView):
+#     model = User
+#     template_name = 'users/register.html'
+#     form_class = UserRegisterForm
+#     success_url = reverse_lazy('users:login')
+#
+#     def get_context_data(self, **kwargs):
+#         context = super(GeekShopRegister, self).get_context_data()
+#         context['title'] = 'GeekShop - Регистрация'
+#         return context
+#
+#
+# class GeekShopProfile(UpdateView):
+#     model = User
+#     template_name = 'users/profile.html'
+#     form_class = UserProfileForm
+#     # success_url = reverse_lazy('users:profile')
+#
+#     def get_context_data(self, object_list=None, **kwargs):
+#         context = super(GeekShopProfile, self).get_context_data()
+#         context.update({
+#             'title': 'GeekShop - Личный кабинет',
+#             'baskets': Basket.objects.filter(user=self.request.user)
+#         })
+#         # context['title'] = 'GeekShop - Личный кабинет'
+#         return context
+#
+#     def get_success_url(self):
+#         return reverse_lazy('users:profile', args=(self.object.id,))
+#
+#     @method_decorator(login_required())
+#     def dispatch(self, request, *args, **kwargs):
+#         return super(GeekShopProfile, self).dispatch(request, *args, **kwargs)
 
 
 class GeekShopLogout(LogoutView):
     next_page = 'index'
-
-
-# def login(request):
-#     if request.method == 'POST':
-#         form = UserLoginForm(data=request.POST)
-#         if form.is_valid():
-#             username = request.POST['username']
-#             password = request.POST['password']
-#             user = auth.authenticate(username=username, password=password)
-#             if user and user.is_active:
-#                 auth.login(request, user)
-#                 return HttpResponseRedirect(reverse('index'))
-#     else:
-#         form = UserLoginForm()
-#     context = {
-#         'title': 'GeekShop - Авторизация',
-#         'form': form,
-#     }
-#     return render(request, 'users/login.html', context)
-
-
-# def register(request):
-#     if request.method == 'POST':
-#         form = UserRegisterForm(data=request.POST)
-#         if form.is_valid():
-#             form.save()
-#             messages.success(request, 'Вы успешно зарегестрировались!')
-#             return HttpResponseRedirect(reverse('users:login'))
-#     else:
-#         form = UserRegisterForm()
-#     context = {
-#         'title': 'GeekShop - Регистрация',
-#         'form': form
-#     }
-#     return render(request, 'users/register.html', context)
-
-# @login_required()
-# def profile(request):
-#     user = request.user
-#     if request.method == 'POST':
-#         form = UserProfileForm(data=request.POST, files=request.FILES, instance=user)
-#         if form.is_valid():
-#             form.save()
-#             return HttpResponseRedirect(reverse('users:profile'))
-#         else:
-#             print(form.errors)
-#     else:
-#         form = UserProfileForm(instance=user)
-#     context = {
-#         'title': 'GeekShop - Личный кабинет',
-#         'form': form,
-#         'baskets': Basket.objects.filter(user=user)
-#     }
-#     return render(request, 'users/profile.html', context)
-
-
-# def logout(request):
-#     auth.logout(request)
-#     return HttpResponseRedirect(reverse('index'))
