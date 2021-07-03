acc249ede3b45303811d8ecf5dd13a363f39ca6b
Vitaly <kazancevvitallijj@gmail.com>
2021-07-03 12:24:32 +0800

    context_processors.py for basket

--- geekshop/geekshop/settings.py
+++ geekshop/geekshop/settings.py
@@ -150,6 +150,6 @@ DOMAIN_NAME = 'http://localhost:8000'
 
 EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_HOST_USER = 'ffabulariss2020@gmail.com'
-EMAIL_HOST_PASSWORD = 'superpuperpassword'
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
